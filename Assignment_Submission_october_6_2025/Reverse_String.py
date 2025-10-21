2c6e84e90da2e22e61d977434606ba1e4377c901
Ishita Reddy Annreddy <iannreddy@gmail.com>
2025-10-20 22:55:36 -0400

    Cleaned up branch to keep only October_20 assignment folder

--- Assignment_Submission_october_6_2025/Reverse_String.py
+++
@@ -1,6 +0,0 @@
-def reverse_string(s: str) -> str:
-    return s[::-1]
-
-if __name__ == "__main__":
-    text = input("Enter a string to reverse: ")
-    print("Reversed string:", reverse_string(text))
